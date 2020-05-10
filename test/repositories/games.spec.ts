3eedcdc313cad43cd03bf2a6d1c4d4701d38fc00
Pedro Sousa <pedronsousa@gmail.com>
2020-05-10 16:20:22 +0100

    getById returns null if none found

--- test/repositories/games.spec.ts
+++ test/repositories/games.spec.ts
@@ -14,6 +14,7 @@ it("insert a Game", async () => {
 
   const gameToInsert = {
     userId: "id2",
+    something: "else",
   }
 
   const gameId = await getRightAction(gamesRepository.insert(gameToInsert), environment)
@@ -23,4 +24,22 @@ it("insert a Game", async () => {
   mongoServer.stop()
 
   expect(insertedGame).toMatchObject({ ...gameToInsert, gameId })
+})
+
+describe("getById", () => {
+  it("return null if not found", async () => {
+    const mongoServer = new MongoMemoryServer()
+    const mongoUri = await mongoServer.getUri()
+
+    const dbClient = await connect(mongoUri)
+    const environment = {
+      dbClient,
+    } as any
+
+    const insertedGame = await getRightAction(gamesRepository.getById("some-id"), environment)
+
+    mongoServer.stop()
+
+    expect(insertedGame).toBeNull()
+  })
 })
