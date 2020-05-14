cff04c645232b4e3a4eeec6bc546f79f9fb2f11d
Pedro Sousa <pedronsousa@gmail.com>
2020-05-14 19:54:42 +0100

    Cleanup

--- src/domain/games.ts
+++ src/domain/games.ts
@@ -44,78 +44,61 @@ const determineWordTypes = (words: string[]): BoardWord[] => {
   return shuffle(words.map((word, i) => ({ word, type: types[i], revealed: false })))
 }
 
-const buildBoard = (boardWidth: number, boardHeight: number) => (words: BoardWord[]): BoardWord[][] =>
-  R.range(0, boardHeight).map(r => words.slice(r * boardWidth, r * boardWidth + boardWidth))
+const buildBoard = (boardWidth: number, boardHeight: number) => (words: string[]): BoardWord[][] => {
+  const wordTypes = determineWordTypes(words)
+  return R.range(0, boardHeight).map(r => wordTypes.slice(r * boardWidth, r * boardWidth + boardWidth))
+}
+
+const exists = <E, T, R>(v: T | undefined, f: (v: NonNullable<T>) => R, errMsg: string) =>
+  v ? actionOf(f(v!)) : actionErrorOf<E, R>(new ServiceError(errMsg, ErrorCodes.NOT_FOUND))
+
+const insertGame = (userId: string): DomainPort<BoardWord[][], CodeNamesGame> => board =>
+  withEnv(({ adapters: { gamesRepository, uuid, currentUtcDateTime } }) =>
+    adapt(
+      gamesRepository.insert({
+        gameId: uuid(),
+        timestamp: currentUtcDateTime().format("YYYY-MM-DD HH:mm:ss"),
+        userId,
+        players: [{ userId }],
+        state: GameStates.idle,
+        turn: Teams.red,
+        board,
+      }),
+    ),
+  )
 
 export const create: DomainPort<CreateGameInput, CreateGameOutput> = ({ userId, language }) =>
-  withEnv(
-    ({
-      config: { boardWidth, boardHeight },
-      adapters: { gamesRepository, wordsRepository, uuid, currentUtcDateTime },
-    }) =>
-      pipe(
-        adapt(wordsRepository.getByLanguage(language)),
-        chain(allWords =>
-          allWords
-            ? actionOf(shuffle(allWords.words).slice(0, boardWidth * boardHeight))
-            : actionErrorOf<DomainEnvironment, string[]>(new ServiceError("Language not found", ErrorCodes.NOT_FOUND)),
-        ),
-        map(determineWordTypes),
-        map(buildBoard(boardWidth, boardHeight)),
-        chain(board =>
-          adapt(
-            gamesRepository.insert({
-              gameId: uuid(),
-              timestamp: currentUtcDateTime().format("YYYY-MM-DD HH:mm:ss"),
-              userId,
-              players: [{ userId }],
-              state: GameStates.idle,
-              turn: Teams.red,
-              board,
-            }),
-          ),
-        ),
+  withEnv(({ config: { boardWidth, boardHeight }, adapters: { wordsRepository } }) =>
+    pipe(
+      adapt(wordsRepository.getByLanguage(language)),
+      chain(allWords =>
+        exists(allWords, a => shuffle(a.words).slice(0, boardWidth * boardHeight), `Language '${language}' not found`),
       ),
+      map(buildBoard(boardWidth, boardHeight)),
+      chain(insertGame(userId)),
+    ),
   )
 
 export const join: DomainPort<JoinGameInput, JoinGameOutput> = ({ gameId, userId }) =>
   withEnv(({ adapters: { gamesRepository } }) =>
     pipe(
       adapt(gamesRepository.getById(gameId)),
-      chain(game =>
-        game
-          ? actionOf(addPlayer(userId)(game))
-          : actionErrorOf<DomainEnvironment, CodeNamesGame>(
-              new ServiceError(`Game '${gameId}' does not exist`, ErrorCodes.NOT_FOUND),
-            ),
-      ),
+      chain(game => exists(game, addPlayer(userId), `Game '${gameId}' does not exist`)),
       chain(game => adapt(gamesRepository.update(game))),
     ),
   )
 
+const reveal = (w: BoardWord) => ({ ...w, revealed: true })
 const revealWordAction = (row: number, col: number) => (game: CodeNamesGame) => ({
   ...game,
-  board: update2dCell(game.board)(
-    w => ({
-      ...w,
-      revealed: true,
-    }),
-    row,
-    col,
-  ),
+  board: update2dCell(game.board)(reveal, row, col),
 })
 
 export const revealWord: DomainPort<RevealWordInput, RevealWordOutput> = ({ gameId, row, col }) =>
   withEnv(({ adapters: { gamesRepository } }) =>
     pipe(
       adapt(gamesRepository.getById(gameId)),
-      chain(game =>
-        game
-          ? actionOf(revealWordAction(row, col)(game))
-          : actionErrorOf<DomainEnvironment, CodeNamesGame>(
-              new ServiceError(`Game '${gameId}' does not exist`, ErrorCodes.NOT_FOUND),
-            ),
-      ),
+      chain(game => exists(game, revealWordAction(row, col), `Game '${gameId}' does not exist`)),
       chain(game => adapt(gamesRepository.update(game))),
     ),
   )
@@ -129,13 +112,7 @@ export const changeTurn: DomainPort<ChangeTurnInput, ChangeTurnOutput> = ({ game
   withEnv(({ adapters: { gamesRepository } }) =>
     pipe(
       adapt(gamesRepository.getById(gameId)),
-      chain(game =>
-        game
-          ? actionOf(changeTurnAction(game))
-          : actionErrorOf<DomainEnvironment, CodeNamesGame>(
-              new ServiceError(`Game '${gameId}' does not exist`, ErrorCodes.NOT_FOUND),
-            ),
-      ),
+      chain(game => exists(game, changeTurnAction, `Game '${gameId}' does not exist`)),
       chain(game => adapt(gamesRepository.update(game))),
     ),
   )
