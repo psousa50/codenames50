5cc28e1c73a1f706c8e520a497bc1a331b80d4d8
Pedro Sousa <pedronsousa@gmail.com>
2020-06-03 08:33:51 +0100

    Refactor new game button

--- src/components/SetupGameView.tsx
+++ src/components/SetupGameView.tsx
@@ -1,8 +1,20 @@
-import { Button, Grid } from "@material-ui/core"
+import { Button, Grid, makeStyles, Theme } from "@material-ui/core"
 import React from "react"
 import { CodeNamesGame, GameStates, Teams } from "../codenames-core/models"
 import * as GameRules from "../codenames-core/rules"
 import { TeamsView } from "./TeamsView"
+
+const useStyles = makeStyles((theme: Theme) => ({
+  buttons: {
+    display: "flex",
+    flow: 1,
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginTop: theme.spacing(3),
+  },
+}))
 
 interface SetupGameViewProps {
   userId: string
@@ -21,21 +33,27 @@ export const SetupGameView: React.FC<SetupGameViewProps> = ({
   startGame,
   nextGame,
 }) => {
+  const classes = useStyles()
+
   const canStartGame = GameRules.startGame(game) === undefined
 
   return (
     <Grid container spacing={0} direction="column" alignItems="center" justify="center">
       <TeamsView userId={userId} game={game} joinTeam={joinTeam} setSpyMaster={setSpyMaster} />
-      <div style={{ marginTop: "30px" }}></div>
-      {game.state === GameStates.idle ? (
+      <div className={classes.buttons}>
         <Button variant="contained" disabled={!canStartGame} size="small" color="primary" onClick={() => startGame()}>
           Start Game
         </Button>
-      ) : (
-        <Button variant="contained" size="small" color="primary" onClick={() => nextGame()}>
+        <Button
+          disabled={game.state === GameStates.idle}
+          variant="contained"
+          size="small"
+          color="primary"
+          onClick={() => nextGame()}
+        >
           New Game
         </Button>
-      )}
+      </div>
     </Grid>
   )
 }
