171647a1398d1e0c3a47a450f71c2108bc1c539c
Pedro Sousa <pedronsousa@gmail.com>
2020-06-16 09:37:29 +0100

    Add infra for api

---
+++ infra/codenames50-api.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+APP_NAME=codenames50-api
+
+heroku create $APP_NAME --remote heroku-api --region eu
+heroku buildpacks:add heroku/nodejs --app $APP_NAME
+heroku buildpacks:add https://github.com/heroku/heroku-buildpack-multi-procfile --app $APP_NAME
+heroku addons:create mongolab:sandbox --app $APP_NAME
+
+heroku config:set PROCFILE=src/apps/codenames50-server/Procfile --app $APP_NAME
+heroku config:set BUILD_ENV=api --app $APP_NAME
