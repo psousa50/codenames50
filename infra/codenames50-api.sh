9f43254ffb9c616559333924bdae0243edb2f84d
Pedro Sousa <pedronsousa@gmail.com>
2020-06-22 10:39:25 +0100

    Rename src to projects

--- infra/codenames50-api.sh
+++ infra/codenames50-api.sh
@@ -7,5 +7,5 @@ heroku buildpacks:add heroku/nodejs --app $APP_NAME
 heroku buildpacks:add https://github.com/heroku/heroku-buildpack-multi-procfile --app $APP_NAME
 heroku addons:create mongolab:sandbox --app $APP_NAME
 
-heroku config:set PROCFILE=src/apps/codenames50-server/Procfile --app $APP_NAME
+heroku config:set PROCFILE=projects/apps/codenames50-server/Procfile --app $APP_NAME
 heroku config:set BUILD_ENV=api --app $APP_NAME
