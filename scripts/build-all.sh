a8bd827cf151b2d7765d7631e73f4ea064d82be0
Pedro Sousa <pedronsousa@gmail.com>
2020-06-27 19:00:27 +0100

    Remove build scripts

--- scripts/build-all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIR="$(cd "$(dirname "$0")" && pwd)"
-
-$DIR/build-packages.sh
-
-yarn workspace codenames50-server exec yarn build
-yarn workspace codenames50-react exec yarn build
--- scripts/build-packages.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-yarn workspace @psousa50/shared exec yarn build
-yarn workspace codenames50-core exec yarn build
-yarn workspace codenames50-messaging exec yarn build
