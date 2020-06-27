6d5e7ffbd5ef818981a4f7b9d68cdab447db5ba1
Pedro Sousa <pedronsousa@gmail.com>
2020-06-27 19:38:28 +0100

    Add build-web

---
+++ scripts/build-web.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+yarn workspace @psousa50/shared build
+yarn workspace @codenames50/core build
+yarn workspace @codenames50/messaging build
+
+yarn workspace @codenames50/web build
--- scripts/run-ui.sh
+++ scripts/run-ui.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 yarn workspace codenames50-react start:dev
