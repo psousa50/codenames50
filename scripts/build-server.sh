c033e13ee66f64ca36c913907cf76a7db5a4f087
Pedro Sousa <pedronsousa@gmail.com>
2020-06-28 15:34:59 +0100

    Fix build server script

--- scripts/build-server.sh
+++ scripts/build-server.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-yarn workspace @codenames50/core test
-yarn workspace @codenames50/server test
-
 yarn workspace @codenames50/core build
 yarn workspace @codenames50/messaging build
-
 yarn workspace @codenames50/server build
+
+yarn workspace @codenames50/core test
+yarn workspace @codenames50/server test
