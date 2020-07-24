b6fe492baaebd52e1955ec5db3875718ead12026
Pedro Sousa <pedronsousa@gmail.com>
2020-07-24 18:18:20 +0100

    Remove socket events logs

--- packages/web/src/utils/socketMessaging.ts
+++ packages/web/src/utils/socketMessaging.ts
@@ -33,18 +33,6 @@ const addMessageHandler = (socket: SocketIOClient.Socket) => (handler: Messages.
 export const buildSocketMessaging = (uri: string) => {
   const socket = io(uri, { autoConnect: false })
 
-  socket.on("connect", () => console.log("connect"))
-  socket.on("disconnect", () => console.log("disconnect"))
-  socket.on("connect_error", () => console.log("connect_error"))
-  socket.on("connect_timeout", () => console.log("connect_timeout"))
-  socket.on("reconnect_attempt", () => console.log("reconnect_attempt"))
-  socket.on("reconnect_error", () => console.log("reconnect_error"))
-  socket.on("reconnect_failed", () => console.log("reconnect_failed"))
-  socket.on("reconnecting", () => console.log("reconnecting"))
-  socket.on("reconnect", () => console.log("reconnect"))
-  socket.on("ping", () => console.log("ping"))
-  socket.on("pong", () => console.log("pong"))
-
   return {
     connect: connect(socket),
     emitMessage: emitMessage(socket),
