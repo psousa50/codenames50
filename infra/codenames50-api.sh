07112b5ef67e2c516511c578d0090b180c71b6fe
Pedro Sousa <pedronsousa@gmail.com>
2024-11-30 12:28:51 +0000

    improve infra

--- infra/codenames50-api.sh
+++ infra/codenames50-api.sh
@@ -5,7 +5,7 @@ APP_NAME=codenames50-api
 heroku create $APP_NAME --remote heroku-api --region eu
 heroku buildpacks:add heroku/nodejs --app $APP_NAME
 heroku buildpacks:add https://github.com/heroku/heroku-buildpack-multi-procfile --app $APP_NAME
-heroku addons:create mongolab:sandbox --app $APP_NAME
 
 heroku config:set PROCFILE=packages/server/Procfile --app $APP_NAME
 heroku config:set BUILD_ENV=api --app $APP_NAME
+heroku config:set MONGODB_URI="mongodb+srv://$MONGO_DB_USER:$MONGO_DB_PASS@$MONGO_DB_NAME/codenames50?retryWrites=true&w=majority" --app $APP_NAME
