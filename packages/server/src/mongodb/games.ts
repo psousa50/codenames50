26ced1a8056f7dc4fc9e5adf7f42db25dc9b3b74
Pedro Sousa <pedronsousa@gmail.com>
2025-05-30 16:43:21 +0100

    chore: enhance build scripts, update MongoDB dependency, and refactor database operations for improved performance

--- packages/server/src/mongodb/games.ts
+++ packages/server/src/mongodb/games.ts
@@ -4,22 +4,30 @@ import { MongoEnvironment } from "./adapters"
 
 const GAMES = "Games"
 
-const insert = ({ dbClient }: MongoEnvironment) => (game: GameModels.CodeNamesGame) =>
-  dbClient
-    .db()
-    .collection<GameModels.CodeNamesGame>(GAMES)
-    .insertOne(game)
-    .then(r => r.ops[0])
+const insert =
+  ({ dbClient }: MongoEnvironment) =>
+  async (game: GameModels.CodeNamesGame) => {
+    const result = await dbClient.db().collection<GameModels.CodeNamesGame>(GAMES).insertOne(game)
 
-const update = ({ dbClient }: MongoEnvironment) => (game: GameModels.CodeNamesGame) =>
-  dbClient
-    .db()
-    .collection<GameModels.CodeNamesGame>(GAMES)
-    .findOneAndUpdate({ gameId: game.gameId }, { $set: game }, { returnOriginal: false })
-    .then(r => r.value || game)
+    // In v6.x, we need to fetch the inserted document manually if we need the full object
+    return dbClient.db().collection<GameModels.CodeNamesGame>(GAMES).findOne({ _id: result.insertedId }) || game
+  }
 
-const getById = ({ dbClient }: MongoEnvironment) => (gameId: UUID) =>
-  dbClient.db().collection<GameModels.CodeNamesGame>(GAMES).findOne({ gameId: gameId })
+const update =
+  ({ dbClient }: MongoEnvironment) =>
+  async (game: GameModels.CodeNamesGame) => {
+    const result = await dbClient
+      .db()
+      .collection<GameModels.CodeNamesGame>(GAMES)
+      .findOneAndUpdate({ gameId: game.gameId }, { $set: game }, { returnDocument: "after" })
+
+    return result || game
+  }
+
+const getById =
+  ({ dbClient }: MongoEnvironment) =>
+  (gameId: UUID) =>
+    dbClient.db().collection<GameModels.CodeNamesGame>(GAMES).findOne({ gameId: gameId })
 
 export const gamesMongoDbPorts = {
   insert,
--- packages/server/src/mongodb/main.ts
+++ packages/server/src/mongodb/main.ts
@@ -1,11 +1,10 @@
-import { FilterQuery, MongoClient } from "mongodb"
+import { Filter, MongoClient } from "mongodb"
 
 type Stringable = {
   toString: () => string
 }
 
-export const connect = (mongoDbUri: string) =>
-  MongoClient.connect(mongoDbUri, { useNewUrlParser: true, useUnifiedTopology: true })
+export const connect = (mongoDbUri: string) => MongoClient.connect(mongoDbUri)
 
 export const disconnect = (client: MongoClient) => client.close()
 
@@ -16,21 +15,25 @@ export function getById<T>(collection: string) {
     client
       .db()
       .collection<T>(collection)
-      .findOne({ _id: id.toString() } as FilterQuery<T>)
+      .findOne({ _id: id.toString() } as Filter<T>)
 }
 
 export function get<T>(collection: string) {
-  return (query: FilterQuery<unknown> = {}) => (client: MongoClient) => {
-    return client.db().collection<T>(collection).find(query).toArray()
-  }
+  return (query: Filter<unknown> = {}) =>
+    (client: MongoClient) => {
+      return client.db().collection<T>(collection).find(query).toArray()
+    }
 }
 
-export const upsertManyById = <T extends { _id: string }>(collection: string) => (data: T[]) =>
-  upsertMany(collection)(data, (item: T) => ({ _id: item._id } as FilterQuery<T>))
+export const upsertManyById =
+  <T extends { _id: string }>(collection: string) =>
+  (data: T[]) =>
+    upsertMany(collection)(data, (item: T) => ({ _id: item._id }) as Filter<T>)
 
-const upsertMany = (collection: string) => <T>(data: T[], filter: (item: T) => FilterQuery<T> = _ => ({})) => (
-  client: MongoClient,
-) =>
-  Promise.all(
-    data.map(item => client.db().collection(collection).updateOne(filter(item), { $set: item }, { upsert: true })),
-  )
+const upsertMany =
+  (collection: string) =>
+  <T>(data: T[], filter: (item: T) => Filter<T> = _ => ({})) =>
+  (client: MongoClient) =>
+    Promise.all(
+      data.map(item => client.db().collection(collection).updateOne(filter(item), { $set: item }, { upsert: true })),
+    )
