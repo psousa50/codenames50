a2f3c82ca87d39e36a2a50fe69620ee3d4eb0e94
Pedro Sousa <pedronsousa@gmail.com>
2025-05-30 22:05:24 +0100

    Temporarily allow all origins for Socket.io debugging

--- packages/server/src/sockets/main.ts
+++ packages/server/src/sockets/main.ts
@@ -5,16 +5,7 @@ import { socketHandler } from "./handlers"
 
 export const createSocketsApplication = (server: Server, allowedOrigins: string[]) => {
   const io = socketIo(server, {
-    origins: allowedOrigins,
-    handlePreflightRequest: (req, res) => {
-      const headers = {
-        "Access-Control-Allow-Headers": "Content-Type, Authorization",
-        "Access-Control-Allow-Origin": req.headers.origin,
-        "Access-Control-Allow-Credentials": "true",
-      }
-      res.writeHead(200, headers)
-      res.end()
-    },
+    origins: "*:*",
   })
 
   return io
