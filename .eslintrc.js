3bdf56ae165e927fd22c09ced742bd52c08dd899
Pedro Sousa <pedronsousa@gmail.com>
2025-05-30 16:22:36 +0100

    chore: update ESLint configuration and dependencies, remove unused lerna options, and upgrade various packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,38 @@
-module.exports = {
-  root: true,
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:jest/recommended"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "jest"],
-  rules: {
-    quotes: ["error", "double"],
-    semi: ["error", "never"],
-    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "_*" }],
-    "@typescript-eslint/explicit-module-boundary-types": ["off"],
+const { FlatCompat } = require("@eslint/eslintrc")
+const js = require("@eslint/js")
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+})
+
+module.exports = [
+  {
+    ignores: ["**/dist/**", "**/node_modules/**", "**/.build-cache/**"],
   },
-  overrides: [
-    {
-      files: ["**/test/**"],
-      rules: {
-        "@typescript-eslint/no-explicit-any": ["off"],
+  ...compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"),
+  {
+    languageOptions: {
+      parser: require("@typescript-eslint/parser"),
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
       },
     },
-  ],
-}
+    plugins: {
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+    },
+    rules: {
+      quotes: ["error", "double"],
+      semi: ["error", "never"],
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "_*" }],
+      "@typescript-eslint/explicit-module-boundary-types": ["off"],
+    },
+  },
+  {
+    files: ["**/test/**"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": ["off"],
+    },
+  },
+]
