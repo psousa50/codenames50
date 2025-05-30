ab1511a790d7f72ccc2f6ba8b8d0a7608580e699
Pedro Sousa <pedronsousa@gmail.com>
2025-05-30 20:37:42 +0100

    Fix Socket.io CORS configuration for v2

--- packages/server/src/sockets/main.ts
+++ packages/server/src/sockets/main.ts
@@ -6,6 +6,15 @@ import { socketHandler } from "./handlers"
 export const createSocketsApplication = (server: Server, allowedOrigins: string[]) => {
   const io = socketIo(server, {
     origins: allowedOrigins,
+    handlePreflightRequest: (req, res) => {
+      const headers = {
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Access-Control-Allow-Origin": req.headers.origin,
+        "Access-Control-Allow-Credentials": "true",
+      }
+      res.writeHead(200, headers)
+      res.end()
+    },
   })
 
   return io
